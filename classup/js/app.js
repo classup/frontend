510a2db61c451e21bafaa48148abdaaafb57f12e
rushabh <rusamgr8@gmail.com>
2016-03-26 22:41:56 +0530

    problem solved

--- classup/js/app.js
+++ classup/js/app.js
@@ -9,49 +9,55 @@ this.selectTab = function(setTab){
 this.isSelected = function(checkTab){
     return this.tab === checkTab;
 }
-}
+});
 app.controller("enggclasscontroller",function($scope){
-$scope.enggclass = [{
+$scope.enggclass = {};
+$scope.enggclass =[
+    {
     name : "Sinhal Classes",
     contact : "789654133",
     img : "images/sinhals.jpg"
     },
     {
-    name : "Vidhyalankar Classes"
+    name : "Vidhyalankar Classes",
     contact : "789654123"  ,
     img : "images/sinhals.jpg"
     },
+    {
     name : "Sinhal Classes",
     contact : "789654133",
     img : "images/sinhals.jpg"
     },
     {
-    name : "Vidhyalankar Classes"
+    name : "Vidhyalankar Classes",
     contact : "789654123" ,
     img : "images/sinhals.jpg"
     },
+    {
     name : "Sinhal Classes",
     contact : "789654133",
     img : "images/sinhals.jpg"
     },
     {
-    name : "Vidhyalankar Classes"
+    name : "Vidhyalankar Classes",
     contact : "789654123" ,
     img : "images/sinhals.jpg"
     },
+    {
     name : "Sinhal Classes",
     contact : "789654133",
     img : "images/sinhals.jpg"
     },
     {
-    name : "Vidhyalankar Classes"
+    name : "Vidhyalankar Classes",
     contact : "789654123" ,
     img : "images/sinhals.jpg"
     },
+    {
     name : "Sinhal Classes",
     contact : "789654133",
     img : "images/sinhals.jpg"
     }
-    ];
-    )};
-});
+    ]
+
+    });
